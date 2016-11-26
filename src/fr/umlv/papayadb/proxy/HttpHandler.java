4dbbf8da18e03e03dd53693958302c7de4915899
Hugo Feuillâtre <hfeuilla@etud.u-pem.fr>
2016-11-26 15:32:07 +0100

    modified HttpHandler to listen on port 8080 (needs root permission for port 80

--- src/fr/umlv/papayadb/proxy/HttpHandler.java
+++ src/fr/umlv/papayadb/proxy/HttpHandler.java
@@ -5,33 +5,35 @@ import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.http.HttpServerRequest;
+import io.vertx.ext.web.Router;
 
 class HttpHandler extends AbstractVerticle {
 	
-	private final HttpServer server;
+	private HttpServer server;
 	private final HttpServerOptions options;
 	
 	public HttpHandler() {
-		this.options = new HttpServerOptions().setHost("127.0.0.1");
-		this.server = Vertx.vertx().createHttpServer(this.options);
-		this.server.requestHandler(this::onHTTPRequest);
+		this.options = new HttpServerOptions().setHost("127.0.0.1").setPort(8080);
 	}
 	
 	@Override
 	public void start() throws Exception {
-		super.start();
+		this.server = vertx.createHttpServer(this.options);
+		this.server.requestHandler(this::onHTTPRequest);
+		/*Router router = Router.router(vertx);
+		server.requestHandler(router::accept);*/
+
 		server.listen(res -> {
 			if(res.succeeded()) {
 				System.out.println("HttpServer is up & ready to listen on " + options.getHost() + ":" + options.getPort());
 			} else {
-				System.out.println("HttpServer has failed to start");
+				System.out.println("HttpServer has failed to start : " + res.cause());
 			}
 		});
 	}
 	
 	@Override
 	public void stop() throws Exception {
-		super.stop();
 		server.close(res -> {
 			if(res.succeeded()) {
 				System.out.println("HttpServer is down");

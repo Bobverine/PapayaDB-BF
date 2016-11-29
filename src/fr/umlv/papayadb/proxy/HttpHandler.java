b98bbf0e940d103e230451cbae158393ff0a547e
Rayco <Rayco@172.22.131.157>
2016-11-29 11:04:55 +0100

    TCPClient delete, tcp client directly in httphandler for transmit data

--- src/fr/umlv/papayadb/proxy/HttpHandler.java
+++ src/fr/umlv/papayadb/proxy/HttpHandler.java
@@ -1,31 +1,36 @@
 package fr.umlv.papayadb.proxy;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.Router;
+import io.vertx.core.net.NetClient;
+import io.vertx.core.net.NetClientOptions;
+import io.vertx.core.net.NetSocket;
 
 class HttpHandler extends AbstractVerticle {
 	
 	private HttpServer server;
-	private final HttpServerOptions options;
+	private final HttpServerOptions httpOptions;
+	private NetClient client;
+	private final NetClientOptions tcpOptions;
 	
 	public HttpHandler() {
-		this.options = new HttpServerOptions().setHost("127.0.0.1").setPort(8080).setLogActivity(true);
+		this.httpOptions = new HttpServerOptions().setHost("127.0.0.1").setPort(8080).setLogActivity(true);
+		this.tcpOptions = new NetClientOptions().setConnectTimeout(10000);
 	}
 	
 	@Override
 	public void start() throws Exception {
-		this.server = vertx.createHttpServer(this.options);
+		this.server = vertx.createHttpServer(this.httpOptions);
 		this.server.requestHandler(this::onHTTPRequest);
-		/*Router router = Router.router(vertx);
-		server.requestHandler(router::accept);*/
+		this.client = vertx.createNetClient(this.tcpOptions);
 
 		server.listen(res -> {
 			if(res.succeeded()) {
-				System.out.println("HttpServer is up & ready to listen on " + options.getHost() + ":" + options.getPort());
+				System.out.println("HttpServer is up & ready to listen on " + httpOptions.getHost() + ":" + httpOptions.getPort());
 			} else {
 				System.out.println("HttpServer has failed to start : " + res.cause());
 			}
@@ -45,7 +50,22 @@ class HttpHandler extends AbstractVerticle {
 	
 	public void onHTTPRequest(HttpServerRequest request) {
 		JsonObject json = RequestConverter.UrlToJson(request);
+		Buffer buffer = Buffer.buffer();
+		json.writeToBuffer(buffer);
+		client.connect(7070, "127.0.0.1", res -> {
+			if (res.succeeded()) {
+			    System.out.println("Connected!");
+			    NetSocket socket = res.result();
+			    socket.write(buffer);
+			  } else {
+			    System.out.println("Failed to connect: " + res.cause().getMessage());
+			  }
+			});
 		request.response().end("Hello world");
 	}
 
+	public void transmit(JsonObject json){
+		
+		}
+	
 }

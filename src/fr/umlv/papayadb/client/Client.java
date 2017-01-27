9855dd15f53061bb3df22c7a20074944f65727f6
Rayco <Rayco@10.62.61.149>
2017-01-27 14:36:51 +0100

    Première refonte du client : - le MainClient ne contient plus qu'un main - HttpHandler à été fusionner avec Client qui devient la classe contenant toutes les méthodes du Client - les méthodes de POST/GET/DELETE/ ETC... ne sont plus accèssible en public/package, il faut passer par la méthode executeCommand (beaucoup plus propre Forax a du rager)

--- src/fr/umlv/papayadb/client/Client.java
+++ src/fr/umlv/papayadb/client/Client.java
@@ -1,43 +1,162 @@
 package fr.umlv.papayadb.client;
 
-import fr.umlv.papayadb.api.RequestInterface;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
-public class Client implements RequestInterface{
+public class Client {
+	private String server;
 
-	@Override
-	public boolean createDatabase(String name) {
-		// TODO Auto-generated method stub
-		return false;
+	public Client(String server) {
+		this.server = server;
 	}
 
-	@Override
-	public boolean deleteDatabase(String name) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * Envoi la commande au serveur en fonction de la requpete de l'utilisateur
+	 * (GET, POST ...)
+	 * 
+	 * @param client
+	 *            Client HTTP qui possède les informations du serveur cible
+	 * 
+	 * @param command
+	 *            Commande de l'utilisateur à envoyer
+	 * 
+	 * @return void
+	 */
+	boolean executeCommand(String command) {
+		String[] split = command.split(" ");
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i < split.length; i++) {
+			sb.append(split[i]);
+		}
+		String request = sb.toString();
+		switch (split[0]) {
+		case "help":
+			printCommand();
+			break;
+		case "createDatabase":
+			post(request);
+			break;
+		case "deleteDatabase":
+			delete(request);
+			break;
+		case "selectDatabases":
+			get("/");
+			break;
+		case "insertFileFromLocal":
+			post(request);
+			break;
+		case "deleteFileFromDb":
+			delete(request);
+			break;
+		case "selectFromDatabase":
+			get(request);
+			break;
+		default:
+			return false;
+		}
+		return true;
 	}
 
-	@Override
-	public boolean selectFromDatabase(String name, String options) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * Affiche la liste des commandes possible
+	 * 
+	 * @return void
+	 */
+	private void printCommand() {
+		System.out.println("Voici les commandes possibles : \n" + "Créer une database : createDatabase /:dbname \n"
+				+ "Supprimer une database : deleteDatabase /:dbname \n"
+				+ "Afficher toutes les bases de données : selectDatabases / \n"
+				+ "Insérer dans une base de donnée : insertFileFromLocal /:dbname/:filename \n"
+				+ "Supprimer dans une base de donnée : deleteFileFromDb /:dbname/:filename \n"
+				+ "Afficher une donnée d'une base de donnée : selectFromDatabase /:dbname?filter=[...] \n");
 	}
 
-	@Override
-	public boolean insertFileFromLocal(String dbname, String filename) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * Permet d'envoyer une requête HTTP de type POST
+	 * 
+	 * @param request
+	 *            requête de l'utilisateur à transmettre
+	 */
+	private void post(String request) {
+		Objects.requireNonNull(request);
+		try {
+			CompletableFuture<HttpResponse> response = HttpRequest.create(new URI(this.server))
+					.body(HttpRequest.fromString(request)).POST().responseAsync();
+			HttpResponse r = response.get();
+			System.out.println(r.body(HttpResponse.asString()));
+		} catch (URISyntaxException | InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
 	}
 
-	@Override
-	public boolean deleteFileFromDatabase(String dbname, String filename) {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * Permet d'envoyer une requête HTTP de type GET
+	 * 
+	 * @param request
+	 *            requête de l'utilisateur à transmettre
+	 */
+	private void get(String request) {
+		Objects.requireNonNull(request);
+		try {
+			CompletableFuture<HttpResponse> response = HttpRequest.create(new URI(this.server))
+					.body(HttpRequest.fromString(request)).GET().responseAsync();
+
+			HttpResponse r = response.get();
+			System.out.println(r.body(HttpResponse.asString()));
+		} catch (URISyntaxException | InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
 	}
 
-	@Override
-	public boolean selectDatabases() {
-		// TODO Auto-generated method stub
-		return false;
+	/**
+	 * Permet d'envoyer une requête HTTP de type PUT
+	 * 
+	 * @param request
+	 *            requête de l'utilisateur à transmettre
+	 */
+	// private void put(String request) {
+	// Objects.requireNonNull(request);
+	// try {
+	// CompletableFuture<HttpResponse> response = HttpRequest
+	// .create(new URI(this.server))
+	// .body(HttpRequest.fromString(request))
+	// .PUT()
+	// .responseAsync();
+	//
+	// HttpResponse r = response.get();
+	// System.out.println(r.body(HttpResponse.asString()));
+	// } catch (URISyntaxException | InterruptedException | ExecutionException
+	// e) {
+	// e.printStackTrace();
+	// }
+	// }
+
+	/**
+	 * Permet d'envoyer une requête HTTP de type DELETE
+	 * 
+	 * @param request
+	 *            requête de l'utilisateur à transmettre
+	 */
+	private void delete(String request) {
+		Objects.requireNonNull(request);
+		URL uri;
+		try {
+			uri = new URL(this.server);
+			HttpURLConnection connexion;
+			connexion = (HttpURLConnection) uri.openConnection();
+			connexion.setRequestMethod("DELETE");
+			connexion.connect();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/fr/umlv/papayadb/client/MainClient.java
+++ src/fr/umlv/papayadb/client/MainClient.java
@@ -3,85 +3,23 @@ package fr.umlv.papayadb.client;
 import java.util.Scanner;
 
 public class MainClient {
-	
-	/**
-	 * Affiche la liste des commandes possible
-	 * 
-	 * @return void
-	 */
-	private static void printCommand() {
-		System.out.println("Voici les commandes possibles :");
-		System.out.println("Créer une database : createDatabase /:dbname");
-		System.out.println("Supprimer une database : deleteDatabase /:dbname");
-		System.out.println("Afficher toutes les bases de données : selectDatabases /");
-		System.out.println("Insérer dans une base de donnée : insertFileFromLocal /:dbname/:filename");
-		System.out.println("Supprimer dans une base de donnée : deleteFileFromDb /:dbname/:filename");
-		System.out.println("Afficher une donnée d'une base de donnée : selectFromDatabase /:dbname?filter=[...]");
-	}
-	
-	/**
-	 * Envoi la commande au serveur en fonction de la requpete de l'utilisateur (GET, POST ...)
-	 * 
-	 * @param client
-	 *            Client HTTP qui possède les informations du serveur cible
-	 *            
-	 * @param command
-	 * 			  Commande de l'utilisateur à envoyer
-	 *            
-	 * @return void
-	 */
-	private static void executeCommand(HttpClientHandler client, String command) {
-		String[] split = command.split(" ");
-		StringBuilder sb = new StringBuilder();
-		for(int i = 1; i < split.length; i++) {
-			sb.append(split[i]);
-		}
-		String request = sb.toString();
-		switch(split[0]) {
-			case "help":
-				printCommand();
-				break;
-			case "createDatabase":
-				client.post(request);
-				break;
-			case "deleteDatabase":
-				client.delete(request);
-				break;
-			case "selectDatabases":
-				client.get("/");
-				break;
-			case "insertFileFromLocal":
-				client.post(request);
-				break;
-			case "deleteFileFromDb":
-				client.delete(request);
-				break;
-			case "selectFromDatabase":
-				client.get(request);
-				break;
-			default:
-				System.out.println("Bad request");
-				break;
-		}
-	}
-	
-	
-	
+
 	public static void main(String[] args) {
 		if (args.length != 1) {
 			throw new IllegalArgumentException("Entrez l'adresse suivi du port ex : http://127.0.0.1:8080");
 		}
 		System.out.println(args[0]);
-		HttpClientHandler client = new HttpClientHandler(args[0]);
+		Client client = new Client(args[0]);
 		System.out.println("Tapez votre commande ou tapez help pour afficher les commandes et la syntaxe");
 		Scanner sc = new Scanner(System.in);
 		String command = sc.nextLine();
-		while(!command.contentEquals("exit")) {
-			executeCommand(client , command);
+		while (!command.contentEquals("exit")) {
+			if (!client.executeCommand(command)) {
+				System.out.println("Bad request !");
+			}
 			command = sc.nextLine();
 		}
 		sc.close();
-		
 	}
 
 }
